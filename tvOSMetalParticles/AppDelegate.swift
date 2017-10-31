5852dd6334e230849dc085b2f50b54f9407011cb
Anton Filatov <anton.s.filatov@gmail.com>
2017-10-31 17:53:38 +0100

    apple TV target added

---
+++ tvOSMetalParticles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  tvOSMetalParticles
+//
+//  Created by Anton on 31/10/2017.
+//  Copyright © 2017 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        application.isIdleTimerDisabled = true
+        return true
+    }
+}
+
